0b08b0e1da9554d5a0850f6bb7971dfac71a4d1d
Jacob Hildebrandt <jake@hildy.ca>
2021-05-25 21:46:15 -0700

    Added Input Field Component

---
+++ src/components/InputField/InputField.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ErrorIcon from "../../assets/icons/error-24px.svg";
+import "./InputField.scss";
+
+function InputField({
+  name,
+  label,
+  value,
+  placeholder,
+  onChange,
+  error,
+  type,
+}) {
+  return (
+    <div className="input-field">
+      <label htmlFor={`${name}-${label}`} className="input-field__label">
+        {label}
+      </label>
+      <input
+        className={error ? "input-field__error" : "input-field__input"}
+        name={name}
+        id={`${name}-${label}`}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        type={type || "text"}
+      />
+      {error && (
+        <div className="error">
+          <img src={ErrorIcon} alt="error icon" className="error__icon" />
+          <p className="error__message">{error}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default InputField;
--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import "./UploadPage.scss";
 import TextArea from "./../../components/TextArea/TextArea";
+import InputField from "./../../components/InputField/InputField";
 
 function UploadPage() {
   const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
 
   return (
     <main className="upload-page">
       <h1>Upload Pictures</h1>
-      <form action="" className="upload-form">
-        <input
-          type="text"
-          className="upload-form__name"
+      <form action="" className="upload-form" autoComplete="off">
+        <InputField
+          name="name"
+          label="Name"
           placeholder="Enter your name..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          error=""
         />
         <TextArea
           name="description"
