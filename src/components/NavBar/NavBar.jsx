195b98ab704060b198708224b3d873a1361c3b0f
Jacob Hildebrandt <jake@hildy.ca>
2021-05-26 17:37:24 -0700

    Fixed highlighted item bug on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
 import IconGallery from "./../IconGallery/IconGallery";
@@ -10,35 +10,40 @@ function NavBar() {
   const colorSelected = "#eceff1";
   const colorNotSelected = "#b0bec5";
 
+  useEffect(() => {
+    setSelected(window.location.pathname);
+    console.log(window.location.pathname);
+  }, [window.location.pathname]);
+
   return (
     <div className="navbar">
       <Link
         to="/gallery"
         className="navbar__item"
-        onClick={() => setSelected("gallery")}
+        onClick={() => setSelected("/gallery")}
       >
         <IconGallery
-          fill={selected === "gallery" ? colorSelected : colorNotSelected}
+          fill={selected === "/gallery" ? colorSelected : colorNotSelected}
           size="42"
         />
       </Link>
       <Link
         to="/upload"
         className="navbar__item"
-        onClick={() => setSelected("upload")}
+        onClick={() => setSelected("/upload")}
       >
         <IconUpload
-          fill={selected === "upload" ? colorSelected : colorNotSelected}
+          fill={selected === "/upload" ? colorSelected : colorNotSelected}
           size="42"
         />
       </Link>
       <Link
         to="/qr-code"
         className="navbar__item"
-        onClick={() => setSelected("qr-code")}
+        onClick={() => setSelected("/qr-code")}
       >
         <IconQRCode
-          fill={selected === "qr-code" ? colorSelected : colorNotSelected}
+          fill={selected === "/qr-code" ? colorSelected : colorNotSelected}
           size="42"
         />
       </Link>
